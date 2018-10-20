ff15d8e742881ccd2e8c7ef9c9337ea187c47b7c
imclydeshelton <36463308+robertsoaa@users.noreply.github.com>
2018-10-20 07:13:13 -0400

    second commit

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use("/images", express.static(path.join("backend/images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -42,5 +43,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular","index.html"));
+});
 
 module.exports = app;
